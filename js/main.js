eafc0ceb404f5967103a2e4b8b46f9541b7d9a07
Matt Greenwood <12268867+mjgwood@users.noreply.github.com>
2018-11-22 18:56:06 +0800

    performance optimisations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
 	//Navigation menu scrollTo
 	$('header nav ul li a').click(function(event){
 		event.preventDefault();
@@ -17,71 +16,11 @@ $(document).ready(function(){
 		$(window).scrollTo({top:$("#hero").position().top, left:'0px'}, 1000);		
 	});
 
-
-
-
-
-
-
-
 	//Show & Hide menu on mobile
 	$('.burger_icon').click(function(){
 		$('header nav').toggleClass('show');
 		$('header .burger_icon').toggleClass('active');
 	});
-
-	
-
-
-
-
-
-
-	//wow.js on scroll animations initialization
-	wow = new WOW(
-	    {
-		  animateClass: 'animated',
-		  mobile: false,
-		  offset: 50
-		}
-	);
-	wow.init();
-
-
-
-
-
-
-
-
-	//parallax effect initialization
-	$('.hero').parallax("50%", 0.3);
-
-
-
-
-
-
-
-
-	//Nice scroll initialization
-	$("html").niceScroll({
-		scrollspeed: 50,
-		autohidemode : false,
-		cursorwidth : 8,
-		cursorborderradius: 8,
-		cursorborder : "0",
-		background : "rgba(48, 48, 48, .4)",
-		cursorcolor : '#1f1f1f',
-		zindex : 999
-	});
-
-
-
-
-
-
-
 
 	//Testimonials slider initialization
 	$("#tslider").owlCarousel({
@@ -96,51 +35,38 @@ $(document).ready(function(){
 		transitionStyle : "fade"
 	});
 
-
-
-
-
-
-
 	//Mailchimp subscription form initialization
 	$('#submit_form').submit(function(){
 		$('#mc_submit').attr('disabled', 'disabled');		
-   		processing('icon', 'loading');
+		processing('icon', 'loading');
 	});
 
 	if($('#submit_form').length){
 		//Mailchim Subscription form
 		$('#submit_form').ajaxChimp({
-		    callback: chimpResponce
+			 callback: chimpResponce
 		});
 	}	
 
 	//Mail chimp callback function
 	function chimpResponce(resp) {
-   		if (resp.result === 'success') {   			
-   			processing('loading', 'icon');
+			if (resp.result === 'success') {
+				processing('loading', 'icon');
 			$('#mc_submit').removeAttr('disabled', 'disabled');
-	        $('#submit_form #mc-email').val('');
-   			$('#error_msg').hide();
-   			$('#success_msg').show();
-	    }else{		
-   			processing('loading', 'icon');
-   			$('#success_msg').hide();
-   			$('#error_msg').show();
-	    	$('#mc_submit').removeAttr('disabled', 'disabled');
-	    }
+			  $('#submit_form #mc-email').val('');
+				$('#error_msg').hide();
+				$('#success_msg').show();
+		 } else {
+				processing('loading', 'icon');
+				$('#success_msg').hide();
+				$('#error_msg').show();
+			$('#mc_submit').removeAttr('disabled', 'disabled');
+		}
 	}
 
 	function processing(hide, show){
 			$('#mc_submit i').removeClass(hide).addClass(show);
 	}
-
-
-
-
-
-
-
 
 	//Popup video
 	$('#play_video').click(function(e){
@@ -158,10 +84,5 @@ $(document).ready(function(){
 		$('.about_video').fadeOut(200,function(){
 			$('iframe', this).remove();
 		});
-
 	});
-
-
-
-
 });
